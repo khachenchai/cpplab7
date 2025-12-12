37dfe93405936bf96cd0527e5c1bd86bc0382b9f
khachenchai <jaiklaanda@gmail.com>
2025-12-12 16:02:57 +0700

    Success Lab7_2-1

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,3 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name, studentID;
+    int gear;
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin, name);
+    cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> studentID;
+    gear = (studentID[0] - '0') * 10 + (studentID[1] - '0') - 12;
+    cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cin.ignore();
+    string movie;
+    cout << name << ": ";
+    getline(cin, movie);
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    string day;
+    cout << name << ": ";
+    getline(cin, day);
+    cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n";
+    if (name == "Luke Skywalker") {
+        cout << name << ": " << "May the Force be with you krub\n";
+    } else if (name == "Prayuth-chan") {
+        cout << name << ": " << "We will do as we promised, it'll take not too long...\n";
+    }
+
+    cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+    return 0;
+}
+
+
 //Dialog of the first example is given below.
 
 /*
